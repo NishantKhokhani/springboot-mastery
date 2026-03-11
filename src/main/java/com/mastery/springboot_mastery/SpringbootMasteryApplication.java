915ed4acbbe176da1dab351cbef0a98608e196a3
Nishant Khokhani <nishant.khokhani@skandiam.com>
2026-03-11 19:57:41 +0530

    feat: intial setup of postgreSql config

---
+++ src/main/java/com/mastery/springboot_mastery/SpringbootMasteryApplication.java
@@ -0,0 +1,13 @@
+package com.mastery.springboot_mastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMasteryApplication.class, args);
+	}
+
+}
